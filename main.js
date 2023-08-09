3a9d03525c4691e871e17044d63ea511b74b50b6
Baxtzodjon <141494256+Baxtzodjon@users.noreply.github.com>
2023-08-09 14:03:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// let name = 'Озодбек'
+// let age = 5
+// let bigint = BigInt(565656568906568e+1)
+
+// console.log(typeof bigint)
+// console.log(name)
+// console.log(age)
+
+
+// let ag_name = ''
+
+// let snake_case = 'Snake Case'
+// let camelCase = 'Camel Case'
+
+
+// let bool = false
+
+
+// let name = 'John'
+// let admin = 'Alex'
+// admin = name
+
+// console.log(name);
+// console.log(admin);
+
+// Объявите две переменные: admin и name. Запишите строку "Джон" в переменную name. Скопируйте значение из переменной name в admin. Выведите в консоль admin
+
+//   2. // Баланс пользователя
+// let balance = 25000
+
+// // Траты
+// let car = 4480
+// let food = 890
+
+// // Доходы
+// let invoice = 5500
+// let stock = 4200
+
+// // Итоги
+// let totalBalance = 0
+// // totalBalance = 
+// console.log(totalBalance);
+
+
